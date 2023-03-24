7d05e0f6e339b0e6540675da7ce6426fcc29a1c2
Fabrício Willans <113811396+Fabricio-Willans-97@users.noreply.github.com>
2023-03-24 12:22:45 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -350,40 +350,4 @@ function CriarLivro(titulo, autor, paginas) {
 const livro1 = new CriarLivro('Bomba atomica', 'Perry Mason', 350) 
 
 console.log(livro1) 
-***********************************************/ 
-
-
-
-
-
-
-                //TESTES
-/***********************************************
-/*function porcentagemNum(porcent, num) {  
-    const porcentagem = (porcent / 100) * num
-    return porcentagem
-}
-
-console.log(porcentagemNum(2, 400));*/
-
-/*function porcentagemNum(num1, num2) {  
-    const porcentagem = Math.round((num1 / num2) * 100)
-    return porcentagem
-}
-
-console.log(`${porcentagemNum(9, 68)}%`);*/
-
-/*function imc(peso, altura) {
-    const imc = (peso / (altura * altura)).toFixed(1)
-
-    if (imc < 18.5) {
-        console.log(`Fulano tem ${imc} de imc e esta abaixo do peso`)
-    }
-    else if (imc > 25) {
-        console.log(`Fulano tem ${imc} de imc e esta acima do peso`)
-    }
-    else {
-        console.log(`Fulano tem ${imc} de imc e esta no peso ideal`)
-    }
-}
-imc(70, 1.75)*/
+***********************************************/
