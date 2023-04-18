ff27171ee34c601b9f1ed20a891f4568b764343e
Fabrício Willans <113811396+Fabricio-Willans-97@users.noreply.github.com>
2023-04-18 16:50:14 -0300

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -4,46 +4,61 @@
 
 
 
-/* CALCULAR PORCENTAGEM DE UM VALOR
+/* CALCULAR PORCENTAGEM DE UM VALOR /
 
     function porcentagemNum(porcent, num) {  
         const porcentagem = (porcent / 100) * num
-        return porcentagem
+        console.log(`${porcent}% de ${num} é igual a ${porcentagem}`);
     }
-
-    console.log(porcentagemNum(2, 400));
+    //porcentagem e numero total
+    porcentagemNum(2, 400)
 */
 
 
 
-/* VERIFICAR QUAL A PORCENTAGEM ENTRE DOIS VALORES
+/* VERIFICAR QUAL A PORCENTAGEM ENTRE DOIS VALORES /
 
     function porcentagemNum(num1, num2) {  
         const porcentagem = Math.round((num1 / num2) * 100)
-        return porcentagem
+        console.log(`${num1} de ${num2} é ${porcentagem}%`);
     }
 
-    console.log(`${porcentagemNum(9, 68)}%`);
+    porcentagemNum(9, 68)
 */
 
 
 
-/* INDICE DE MASSA CORPORAL (IMC)
+/* INDICE DE MASSA CORPORAL (IMC) /
 
-    function imc(peso, altura) {
+    function imc(nome, peso, altura) {
         const imc = (peso / (altura * altura)).toFixed(1)
 
         if (imc < 18.5) {
-            console.log(`Fulano tem ${imc} de imc e esta abaixo do peso`)
+            console.log(`${nome} tem ${imc} de imc e esta abaixo do peso`)
         }
         else if (imc > 25) {
-            console.log(`Fulano tem ${imc} de imc e esta acima do peso`)
+            console.log(`${nome} tem ${imc} de imc e esta acima do peso`)
         }
         else {
-            console.log(`Fulano tem ${imc} de imc e esta no peso ideal`)
+            console.log(`${nome} tem ${imc} de imc e esta no peso ideal`)
         }
     }
-    imc(70, 1.75)
+    //Nome | Peso | Altura
+    imc('Fulano', 70, 1.75)
 */
 
 
+
+/* NÚMERO ALEATÓRIO Math.random() /
+
+    let numAleatorio = function(min, max) {
+        min = Math.ceil(min);
+        //Math.ceil() retorna o menor número inteiro maior ou igual a "x", ex: Math.floor( 45.95); //  45
+        max = Math.floor(max);
+        //Math.floor() retorna o menor número inteiro dentre o número "x", ex: Math.ceil(7.004);  // 8
+        console.log(Math.floor(Math.random() * (max - min) + min));
+}
+    // Numero minimo e maximo
+    numAleatorio(-10, 10)
+    
+*/
